5227584a769a8903ce6b40b925c75bfd7c6b22a1
Aaron Haun <aaron@haun.guru>
2023-01-12 00:33:59 -0500

    feat: Extract tests for klippy

--- bin/get-sources.sh
+++ bin/get-sources.sh
@@ -8,6 +8,8 @@ fi
 tar xvf klipper-${klippy_version}.tar.gz --strip-components 1 \
 	--transform 's:lib/hub-ctrl:hub-ctrl:' \
 	--transform 's:scripts/klippy-requirements\.txt:upstream-requirements.txt:' \
+	--transform 's:test/klippy:testcases:' \
 	klipper-${klippy_version}/lib/hub-ctrl/ \
 	klipper-${klippy_version}/klippy \
-	klipper-${klippy_version}/scripts/klippy-requirements.txt
+	klipper-${klippy_version}/scripts/klippy-requirements.txt \
+	klipper-${klippy_version}/test/klippy
