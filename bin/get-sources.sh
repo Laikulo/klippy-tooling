9c0ef5c06606c1a84b338e448ce3656590420f46
Aaron Haun <aaron@haun.guru>
2023-04-27 20:59:45 -0400

    tweak: include docs and config dir in package

--- bin/get-sources.sh
+++ bin/get-sources.sh
@@ -9,10 +9,16 @@ tar xvf klipper-${klippy_version}.tar.gz --strip-components 1 \
 	--transform 's:lib/hub-ctrl:hub-ctrl:' \
 	--transform 's:/test/:/testcases/:' \
 	--transform 's:/test$:/testcases:' \
+	--transform 's:/docs$:/klippy/docs:' \
+	--transform 's:/docs/:/klippy/docs/:' \
+	--transform 's:/config$:/klippy/config:' \
+	--transform 's:/config/:/klippy/config/:' \
 	--transform 's:scripts/klippy-requirements\.txt:upstream-requirements.txt:' \
 	--transform 's:scripts/test_klippy\.py:test_klippy.py:' \
 	klipper-${klippy_version}/lib/hub-ctrl/ \
 	klipper-${klippy_version}/klippy \
 	klipper-${klippy_version}/scripts/test_klippy.py \
 	klipper-${klippy_version}/scripts/klippy-requirements.txt \
+	klipper-${klippy_version}/docs \
+	klipper-${klippy_version}/config \
 	klipper-${klippy_version}/test
