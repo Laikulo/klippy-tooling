c4a72f4f42aa0f723a0264ba477ffa21c00bc29a
Aaron Haun <aaron@haun.guru>
2023-05-04 19:33:39 -0400

    Adjust scripts to be more useful when patching

--- bin/relocate.sh
+++ bin/relocate.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-cp -r upstream/lib/hub-ctrl ./hub-ctrl
-cp -r upstream/test ./testcases
-cp -r upstream/klippy ./
-cp -r upstream/docs ./klippy/docs
-cp -r upstream/config ./klippy/config
-cp upstream/scripts/klippy-requirements.txt upstream-requirements.txt
-cp upstream/scripts/test_klippy.py test_klippy.py
+
+if [[ $1 ]]; then
+	klippy_only=1
+	outdir="$1"
+else
+	outdir=klippy
+fi
+
+if [[ -d $outdir ]]; then
+	rm -rf $outdir
+fi
+
+cp -r upstream/klippy ./"${outdir}"
+cp -r upstream/docs ./"${outdir}"/docs
+cp -r upstream/config ./"${outdir}"/config
+
+if [[ ! $klippy_only ]]; then
+	cp -r upstream/lib/hub-ctrl ./hub-ctrl
+	cp -r upstream/test ./testcases
+	cp upstream/scripts/klippy-requirements.txt upstream-requirements.txt
+	cp upstream/scripts/test_klippy.py test_klippy.py
+fi
