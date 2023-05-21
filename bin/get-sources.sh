8d139b883ce5727d4e3b45ab6dc38390baeb9808
Aaron Haun <aaron@haun.guru>
2023-05-20 21:03:22 -0400

    tweak: use init/fetch to checkout klippy

--- bin/get-sources.sh
+++ bin/get-sources.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 klippy_version="${1:-v0.11.0}"
-klippy_upstream="${2:-http://github.com/klipper3d/klipper.git}"
+klippy_upstream="${2:-https://github.com/Klipper3d/klipper.git}"
 if [[ -d upstream ]]; then
 	rm -rf upstream
 fi
