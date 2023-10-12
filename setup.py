4079b26517a13789e80197c1ceeee565bc61180c
Aaron B. Haun <aaron@haun.guru>
2023-10-11 20:52:44 -0400

    Support for working sans wheels

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 import subprocess, sys, os, logging
 from setuptools.command.build import build, SubCommand
 from setuptools import Command
-from wheel.bdist_wheel import bdist_wheel
 from glob import glob
 import setuptools.command.install as orig_install
 
 
 ## Handling for klipper's building of the C helper and hubctl
+
+custom_cmdclass = {}
 
 class BuildCHelperSubCommand(build):
     def run(self):
@@ -29,21 +30,29 @@ class BuildCHelperSubCommand(build):
                 f"{self.build_lib}/klippy/chelper/c_helper.so",
                 f"{self.build_lib}/klippy/chelper/hub-ctrl"
         ]
+custom_cmdclass['build_klipper_chelper'] = BuildCHelperSubCommand
 
 
 class BuildKlipperCommand(build):
     def __init__(self,dist):
         super().__init__(dist)
         self.sub_commands.append(('build_klipper_chelper', None))
+custom_cmdclass['build'] = BuildKlipperCommand
 
 ## Because the above doesn't trigger bdist_wheel to treat this as non pure-python
 
-class ImpureBdistWheel(bdist_wheel):
-    description = "create an explicitly nonportable wheel"
+try:
+    from wheel.bdist_wheel import bdist_wheel
 
-    def finalize_options(self):
-        super().finalize_options()
-        self.root_is_pure = False
+    class ImpureBdistWheel(bdist_wheel):
+        description = "create an explicitly nonportable wheel"
+
+        def finalize_options(self):
+            super().finalize_options()
+            self.root_is_pure = False
+    custom_cmdclass['bdist_wheel'] = ImpureBdistWheel
+except:
+    pass
 
 ## Similar case with install
 
@@ -53,13 +62,9 @@ class ImpureInstall(orig_install.install):
     def finalize_options(self):
         super().finalize_options()
         self.install_lib = self.install_platlib
+custom_cmdclass['install'] = ImpureInstall
         
 
 setup(
-    cmdclass={
-        'build': BuildKlipperCommand,
-        'build_klipper_chelper': BuildCHelperSubCommand,
-        'bdist_wheel': ImpureBdistWheel,
-        'install': ImpureInstall
-    }
+    cmdclass=custom_cmdclass
 )
