b7639f947d105f732214e5cf0654cf80fc8ef378
Aaron Haun <aaron@haun.guru>
2023-10-15 17:53:49 -0400

    update github-run.sh for newer versions of prepare

--- .github/actions/klippy-build/github-run.sh
+++ .github/actions/klippy-build/github-run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-bin/prepare.sh laikulo-devel https://github.com/Laikulo/klipper.git
-pyproject-build --sdist
+TARGET_REF="$(git ls-remote https://github.com/Laikulo/klipper.git laikulo-devel | cut -f 1)"
+bin/prepare.sh $TARGET_REF
+pyproject-build-3.11 --sdist
